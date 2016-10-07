cbfad4ebcdaaacd50a33e1f885193580caca6b9b
destrooooyer <destrooooyer@outlook.com>
2016-10-07 11:58:18 +0800

    按时打算打打

--- App1/App1/today.xaml.cs
+++ App1/App1/today.xaml.cs
@@ -31,5 +31,64 @@ namespace App1
         {
             pane.splitview.IsPaneOpen = !pane.splitview.IsPaneOpen;
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            initGrid();
+            if (global.File == null)
+                temp_message.Text = "没有打开文件，滚去设置";
+            else
+                temp_message.Text = "已打开 " + global.File.Name;
+        }
+
+        private void initGrid()
+        {
+            if (global.File != null)
+            {
+                temp_message.Text = "已打开 " + global.File.Name;
+
+                grid1.Children.Clear();
+                grid1.RowDefinitions.Clear();
+                grid1.ColumnDefinitions.Clear();
+                InitRows(6, grid1);
+                for (int i = 2; i < 8; i++)
+                {
+                    int j = Convert.ToInt32(DateTime.Now.DayOfWeek) + 1;
+
+                    TextBlock block = new TextBlock();
+
+                    block.Text = global.Strs(global.Str_id[i, j]);
+                    block.Padding = new Thickness(10);
+                    block.TextWrapping = TextWrapping.Wrap;
+                    block.MinHeight = 100;
+
+                    grid1.Children.Add(block);
+                    Grid.SetRow(block, i - 2);
+                    //temp_message.Text = ""+ str_id[i,j] + " "+ System.Text.Encoding.Unicode.GetString(strs[str_id[i, j]]);
+
+                    Border bd = new Border();
+                    grid1.Children.Add(bd);
+                    Grid.SetRow(bd, i - 2);
+
+
+                }
+
+            }
+            else
+            {
+                temp_message.Text = "没有打开文件，滚去设置";
+            }
+        }
+
+        private void InitRows(int rowCount, Grid g)
+        {
+            while (rowCount-- > 0)
+            {
+                RowDefinition rd = new RowDefinition();
+                rd.Height = new GridLength();
+                g.RowDefinitions.Add(rd);
+            }
+        }
     }
 }
